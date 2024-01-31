5bc3754e0342ff4922503f0b665adca57cf7b541
Jeremy DA SILVA <136356801+JeremDasilva@users.noreply.github.com>
2024-01-31 17:24:30 +0100

    First upload

---
+++ My_code/web_app.py
@@ -0,0 +1,148 @@
+#Libraries
+import streamlit as st
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+import pandas as pd
+import numpy as np
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import pickle
+
+sales_df = pd.read_csv('../Datasets/car_price_prediction-preprocess.csv')
+
+def model_cleaner(model):
+    try:
+        model = model.split(' ')
+        return model[0]
+    except :
+        return model
+sales_df['model'] = sales_df['model'].apply(lambda x : model_cleaner(x) )
+sales_df['turbo'] = sales_df['turbo'].replace({0: 'No', 1: 'Yes'})
+
+
+#Landing page and sidebar
+st.title('Used cars market : analysis, prediction and search engine')
+
+st.sidebar.title('Menu')
+
+#Tool 1 : Prediction model
+if st.sidebar.checkbox('Prediction Model'):
+    
+    st.markdown('## Prediction Model')
+    
+    #Preparing the dataset the same way as the prediction model
+    sales_df_prediction = sales_df
+    sales_df_prediction.drop(['turbo', 'wheel', 'cylinders', 'drive_wheels'], axis=1, inplace = True)
+    sales_df_prediction = sales_df_prediction[sales_df_prediction['price'] < 50000]
+    sales_df_prediction = sales_df_prediction[sales_df_prediction['price'] > 1000]
+    sales_df_prediction = sales_df_prediction[sales_df_prediction['levy'] < 2000]
+    sales_df_prediction = sales_df_prediction[sales_df_prediction['mileage'] < 200000]
+    sales_df_prediction = sales_df_prediction[sales_df_prediction['production_year'] > 2000]
+    sales_df_prediction = sales_df_prediction[sales_df_prediction['engine_size'] >= 1]
+    
+    column_order = list(sales_df_prediction.drop(['price'], axis = 1).columns)
+    
+    #User interface for input parameters
+    X_pred = {}
+    
+    options_list_manufacturer = list(sorted(sales_df['manufacturer'].unique()))
+    selected_option_manufacturer = st.sidebar.selectbox('manufacturer', options_list_manufacturer)
+    X_pred['manufacturer'] = selected_option_manufacturer    
+    filtered_df_manufacturer = sales_df[sales_df['manufacturer'] == selected_option_manufacturer]
+
+    options_list_model = list(sorted(filtered_df_manufacturer['model'].unique()))
+    X_pred['model'] = st.sidebar.selectbox('model', options_list_model)
+    
+    for col in sales_df_prediction.drop(['price', 'manufacturer', 'model'], axis=1).columns:
+        if sales_df_prediction[col].dtype in [np.float64, np.int64]:
+            col_value = st.sidebar.slider(col, int(sales_df_prediction[col].min()), int(sales_df_prediction[col].max()), int(sales_df_prediction[col].mean()))
+            X_pred[col] = col_value
+        else:
+            col_value = st.sidebar.selectbox(col, sales_df_prediction[col].unique())
+            X_pred[col] = col_value
+            
+    st.write('I want to estimate the following car :')
+    X_pred_df = pd.DataFrame(X_pred, index = [0])
+    st.write(X_pred_df)
+            
+    #Encoding the user input to feed the model
+    manufacturer_dict = {manufacturer: i for i, manufacturer in enumerate(sales_df_prediction['manufacturer'].unique())}
+    X_pred_df['manufacturer'] = X_pred_df['manufacturer'].map(manufacturer_dict)
+    
+    model_dict = {model: i for i, model in enumerate(sales_df_prediction['model'].unique())}
+    X_pred_df['model'] = X_pred_df['model'].map(model_dict)
+
+    X_pred_df['lether_interior'] = X_pred_df['lether_interior'].replace({'Yes': 1, 'No': 0})
+
+    fuel_type_dict = {fuel_type: i for i, fuel_type in enumerate(sales_df_prediction['fuel_type'].unique())}
+    X_pred_df['fuel_type'] = X_pred_df['fuel_type'].map(fuel_type_dict)
+
+    gearbox_type_dict = {gearbox_type: i for i, gearbox_type in enumerate(sales_df_prediction['gearbox_type'].unique())}
+    X_pred_df['gearbox_type'] = X_pred_df['gearbox_type'].map(gearbox_type_dict)
+
+    color_dict = {color: i for i, color in enumerate(sales_df_prediction['color'].unique())}
+    X_pred_df['color'] = X_pred_df['color'].map(color_dict)
+
+    category_dict = {category: i for i, category in enumerate(sales_df_prediction['category'].unique())}
+    X_pred_df['category'] = X_pred_df['category'].map(category_dict)
+    
+    #Reordering the columns to have them in the same order as the trained model
+    X_pred_df = X_pred_df[column_order]
+
+    #Running the model
+    with open('../Model/ETR_car_sales.pkl', 'rb') as file:
+        model = pickle.load(file)
+    if st.button('Run'):
+        pred = model.predict(X_pred_df)
+        price_estimated = str(pred[0])
+        st.write('This car is estimated $', price_estimated)
+
+#Tool 2 : Data visualization 
+elif st.sidebar.checkbox('Data visualization') :
+    sales_df['turbo'] = sales_df['turbo'].replace({0: 'No', 1: 'Yes'})
+    
+    st.markdown('## Data visualization')
+    
+    options_list = ['Mean Price by Production Year', 
+                    'Distribution of Car Categories', 'c']
+
+    selected_option = st.sidebar.selectbox('What do you want to visualize?', options_list)
+
+    if selected_option == options_list[0]:
+        sales_df_sorted = sales_df.groupby('production_year')['price'].mean().reset_index().sort_values(by='production_year')
+
+        plt.figure(figsize=(10, 6))
+        plt.barh(y=sales_df_sorted['production_year'], width=sales_df_sorted['price'])
+        plt.xlabel('Mean Price')
+        plt.ylabel('Production Year')
+        plt.title('Mean Price by Production Year')
+        st.pyplot()
+
+    elif selected_option == options_list[1]:
+        category_counts = sales_df['category'].value_counts().reset_index()
+        
+        plt.figure(figsize=(10, 6))
+        sns.barplot(x='index', y='category', data=category_counts)
+        plt.xlabel('Count')
+        plt.ylabel('Category')
+        plt.title('Distribution of Car Categories')
+        st.pyplot()      
+    
+
+        
+#Tool 3 : Car finder 
+elif st.sidebar.checkbox('Car Finder') :
+    
+    st.markdown('## Car Finder')
+    
+    options_list_manufacturer = list(sorted(sales_df['manufacturer'].unique()))
+    selected_option_manufacturer = st.sidebar.selectbox('Which Manufacturer are you looking for?', options_list_manufacturer)
+    filtered_df_manufacturer = sales_df[sales_df['manufacturer'] == selected_option_manufacturer]
+
+    options_list_model = list(sorted(filtered_df_manufacturer['model'].unique()))
+    selected_option_model = st.sidebar.selectbox('Which model are you looking for?', options_list_model)
+    filtered_df_model = filtered_df_manufacturer[filtered_df_manufacturer['model'] == selected_option_model]
+
+    st.write(filtered_df_model)
